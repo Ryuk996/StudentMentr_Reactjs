69bf5d52f4f048fa4dbf28744209574dab3a082f
Ryuk996 <thirunavu96@gmail.com>
2021-09-27 14:10:06 +0530

    Added react stud

--- src/CreateMentor.js
+++ src/CreateMentor.js
@@ -34,7 +34,7 @@ export default function CreateMentor(){
     return(
         <div>
       <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Create Product</h1>
+        <h1 class="h3 mb-0 text-gray-800">Create Mentor</h1>
       </div>
       <div className="container">
         <form onSubmit={handlleSubmit}>
--- src/EditMentor.js
+++ src/EditMentor.js
@@ -43,7 +43,7 @@ export default function EditMentor(props){
     return(
         <div>
       <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Create Product</h1>
+        <h1 class="h3 mb-0 text-gray-800">Edit Mentor</h1>
       </div>
       <div className="container">
         <form onSubmit={handlleSubmit}>
